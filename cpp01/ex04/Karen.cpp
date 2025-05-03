716b9943aa1fe216b64ab0dcc5c7227349631aa5
psergioprt <paulo.santossilva@gmail.com>
2025-05-03 19:28:48 +0100

    add ex04 cpp

---
+++ cpp01/ex04/Karen.cpp
@@ -0,0 +1,55 @@
+#include "Karen.hpp"
+
+Karen::Karen()
+{
+	std::cout << "Starting up Karen logger" << std::endl;
+}
+
+Karen::~Karen()
+{
+	std::cout << "Shutting down Karen logger" << std::endl;
+}
+
+void	Karen::debug( void )
+{
+	std::cout << "DEBUG: I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
+}
+
+void	Karen::info( void )
+{
+	std::cout << "INFO: I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
+}
+
+void	Karen::warning( void )
+{
+	std::cout << "WARNING: I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
+}
+
+void	Karen::error( void )
+{
+	std::cout << "ERROR: This is unacceptable, I want to speak to the manager now." << std::endl;
+}
+
+void	Karen::complain( std::string level )
+{
+	typedef	void (Karen::*KarenFunction)();
+
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	KarenFunction functions[] =
+	{
+		&Karen::debug,
+		&Karen::info,
+		&Karen::warning,
+		&Karen::error
+	};
+	int	i = 0;
+	while (i < 4)
+	{
+		if (level == levels[i])
+		{
+			(this->*functions[i])();
+			return ;
+		}
+		i++;
+	}
+}
