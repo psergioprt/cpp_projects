869adf78ab521fec516e53f470e66e53e500cddf
psergioprt <paulo.santossilva@gmail.com>
2025-05-21 15:53:43 +0100

    update cpp04 ex02 main.cpp

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pauldos- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 12:06:40 by pauldos-          #+#    #+#             */
-/*   Updated: 2025/05/20 12:14:29 by pauldos-         ###   ########.fr       */
+/*   Updated: 2025/05/20 19:04:52 by pauldos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,32 @@
 
 int main()
 {
+	Animal a;
+	Animal* a = new Animal();
 
+	const	Animal* animals[4];
+
+	for (int i = 0; i < 2; ++i)
+		animals[i] = new Dog();
+	for (int i = 2; i < 4; ++i)
+		animals[i] = new Cat();
+
+	std::cout << "\n--- Making sounds ---" << std::endl;
+	for (int i = 0; i < 4; ++i)
+		animals[i]->makeSound();
+
+	for (int i = 0; i < 4; ++i)
+		delete animals[i];
+
+	std::cout << "\n--- Deep Copy Check ---" << std::endl;
+	Dog original;
+	original.setIdea(0, "Catch the ball");
+
+	Dog copy = original;
+	original.setIdea(0, "Chew the slipper");
+
+	std::cout << "Original idea: " << original.getIdea(0) << std::endl;
+	std::cout << "copied idea: " << copy.getIdea(0) << std::endl;
+
+	return (0);
 }
