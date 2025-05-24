1986f1d74a43ba6044779128572106dd3190b264
psergioprt <paulo.santossilva@gmail.com>
2025-05-24 17:20:29 +0100

    update cpp01 ex05

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pauldos- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 11:47:34 by pauldos-          #+#    #+#             */
-/*   Updated: 2025/05/24 16:59:43 by pauldos-         ###   ########.fr       */
+/*   Updated: 2025/05/24 17:18:58 by pauldos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,7 @@ int	main(int argc, char *argv[])
 	if (argc == 2)
 	{
 		Karen karen;
-
 		karen.complain(argv[1]);
-		//karen.complain("DEBUG");
-		//karen.complain("INFO");
-		//karen.complain("WARNING");
-		//karen.complain("ERROR");
 	}
 	else
 		std::cout << "Usage: <command> <arg>" << std::endl;
