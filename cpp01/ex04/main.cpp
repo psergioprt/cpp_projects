95e6e7d1f81b47a91c9c1a0ae1d73da04d1a2cae
psergioprt <paulo.santossilva@gmail.com>
2025-05-24 16:08:18 +0100

    update cpp01 ex01

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pauldos- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 10:56:05 by pauldos-          #+#    #+#             */
-/*   Updated: 2025/05/06 10:56:11 by pauldos-         ###   ########.fr       */
+/*   Updated: 2025/05/24 15:32:23 by pauldos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ std::string	replaceAll(const std::string& line, const std::string& s1, const std
 		pos = found + s1.length();
 	}
 	result.append(line, pos);
-	return (result);
+	return result;
 }
 
 int	main(int argc, char *argv[])
@@ -35,7 +35,7 @@ int	main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		std::cerr << "Usage: ./replace <filename> <s1> <s2>" << std::endl;
-		return (1);
+		return 1;
 	}
 	std::string filename = argv[1];
 	std::string s1 = argv[2];
@@ -43,32 +43,28 @@ int	main(int argc, char *argv[])
 	if (s1.empty())
 	{
 		std::cerr << "Error: s1 must not be empty!" << std::endl;
-		return (1);
+		return 1;
 	}
 	
-	// Open input file
 	std::ifstream infile(filename.c_str());
 	if (!infile)
 	{
 		std::cerr << "Error: could not open input file." << std::endl;
-		return (1);
+		return 1;
 	}
-
-	// Open output file
+	
 	std::ofstream outfile((filename + ".replace").c_str());
 	if (!outfile)
 	{
 		std::cerr << "Error: could not open/create output file." << std::endl;
-		return (1);
+		return 1;
 	}
 
-	// Read entire file into a string
 	std::string line;
 	while (std::getline(infile, line))
 	{
-		//TODO: implement search and replace for this line
 		std::string new_line = replaceAll(line, s1, s2);
 		outfile << new_line << std::endl;
 	}
-	return (0);
+	return 0;
 }
