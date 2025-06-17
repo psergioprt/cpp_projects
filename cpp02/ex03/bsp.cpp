9ddc03b5af56368e8aadd620683514eb73c81865
psergioprt <paulo.santossilva@gmail.com>
2025-06-17 17:35:43 +0100

    update cpp02 ex03 bsp and main

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: pauldos- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 10:31:21 by pauldos-          #+#    #+#             */
-/*   Updated: 2025/06/17 11:50:19 by pauldos-         ###   ########.fr       */
+/*   Updated: 2025/06/17 15:13:45 by pauldos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 static Fixed area(Point const& p1, Point const& p2, Point const& p3)
 {
-	//(x1 * (y2 - y3)) + (x2 * (y3 - y1)) + (x3 * (y1 - y2))
 	Fixed term1 = p1.getX() * (p2.getY() - p3.getY());
 	Fixed term2 = p2.getX() * (p3.getY() - p1.getY());
 	Fixed term3 = p3.getX() * (p1.getY() - p2.getY());
@@ -25,25 +24,20 @@ static Fixed area(Point const& p1, Point const& p2, Point const& p3)
 	return result / Fixed(2); // OR: Fixed(0.5f) * result;
 }
 
-bool	bsp( Point const a, Point const b, Point const c, Point const point)
+std::string	bsp( Point const a, Point const b, Point const c, Point const point)
 {
 	Fixed totalArea = area(a, b, c).abs();
 	Fixed area1 = area(point, b, c).abs();
 	Fixed area2 = area(a, point, c).abs();
 	Fixed area3 = area(a, b, point).abs();
+	const Fixed tolerance = Fixed(0.01f); 
 
-	std::cout << "Total: " << totalArea << "\n";
+	std::cout << "Total: " << totalArea << std::endl;
 	std::cout << "A1: " << area1 << ", A2: " << area2 << ", A3: " << area3 << std::endl;
 
-
-	//If any of the areas is zero, point is on edge or vertex
 	if (area1 == 0 || area2 == 0 || area3 == 0)
-		return false;
-
-	// Check if the sum of the small areas is close enough to the total area
-	//const Fixed tolerance = Fixed(0.01f); // small tolerance for floating-point comparison
-	const Fixed tolerance = Fixed(0.01f); 
-	if ((area1 + area2 + area3 - totalArea).abs() < tolerance)
-        	return true;
-	return false;
+		return "On edge or vortex";
+	else if ((area1 + area2 + area3 - totalArea).abs() < tolerance)
+		return "Inside";
+	return "outside";
 }
--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,33 +6,26 @@
 /*   By: pauldos- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 10:33:11 by pauldos-          #+#    #+#             */
-/*   Updated: 2025/06/17 11:41:38 by pauldos-         ###   ########.fr       */
+/*   Updated: 2025/06/17 15:17:38 by pauldos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Point.hpp"
 
-bool	bsp( Point const a, Point const b, Point const c, Point const point);
+std::string	bsp( Point const a, Point const b, Point const c, Point const point);
 
 int	main()
 {
-	/*Point	a(0, 0);
+	Point	a(0, 0);
 	Point	b(10, 0);
-	Point	c(0, 10);*/
-	Point	a (2, 0);
-	Point	b (8, 0);
-	Point	c (5, 8);
-	
-	Point inside(4, 4);
-	Point onEdge(5, 8);
-	Point outside(1, 5);
+	Point	c(0, 10);
 
-	/*Point inside(2, 2);
+	Point inside(2, 2);
 	Point onEdge(0, 5);
-	Point outside(10, 10);*/
+	Point outside(10, 10);
 
-	std::cout << "Inside? " << bsp(a, b, c, inside) << std::endl;   // true
-	std::cout << "On edge? " << bsp(a, b, c, onEdge) << std::endl;  // false
-	std::cout << "Outside? " << bsp(a, b, c, outside) << std::endl; // false
+	std::cout << "Inside? " << bsp(a, b, c, inside) << std::endl;
+	std::cout << "On edge? " << bsp(a, b, c, onEdge) << std::endl;
+	std::cout << "Outside? " << bsp(a, b, c, outside) << std::endl;
 }
