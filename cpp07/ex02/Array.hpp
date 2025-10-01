5668538a44d63811c11d3afb238f7efe1252e647
psergioprt <paulo.santossilva@gmail.com>
2025-10-01 17:21:15 +0100

    update cpp07 ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: pauldos- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 11:04:33 by pauldos-          #+#    #+#             */
-/*   Updated: 2025/10/01 12:40:45 by pauldos-         ###   ########.fr       */
+/*   Updated: 2025/10/01 17:12:32 by pauldos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,45 @@ class Array
 		T* _data;
 
 	public:
-		Array();
-		Array(unsigned int n) : _size(n), _data(new T[n]()){}
-		~Array() { delete [] _data;}
-		Array(const Array& other);
+		Array() : _size(0), _data(NULL){ std::cout << "Default Constructor called\n"; }
+		Array(unsigned int n) : _size(n), _data(new T[n]()){ std::cout << "Constructor called with size 5\n"; }
+		~Array() { std::cout << "Destructor called\n"; delete [] _data; }
+		Array(const Array &other) : _size(other._size), _data(NULL)
+		{
+			std::cout << "Copy constructor called with size: " << _size << std::endl;
+			if (_size > 0)
+			{
+				_data = new T[_size];
+				for (unsigned int i = 0; i < _size; i++)
+					this->_data[i] = other._data[i];
+			}
+		}
 		Array& operator=(const Array& other)
 		{
-			if (*this != &other)
-				this = other;
+			std::cout << "Assignement operator called with size " << _size << std::endl; 
+			if (this != &other)
+			{
+				delete [] _data;
+				_size = other._size;
+				_data = new T[_size]();
+				for (unsigned int i = 0; i < _size; i++)
+				{
+					_data[i] = other._data[i];
+				}
+			}
 			return *this;
 		}
-
+		T& operator[](unsigned int index)
+		{
+			if (index >= _size)
+				throw std::out_of_range("Index out of bounds");
+			return _data[index];
+		}
+		const T& operator[](unsigned int index) const
+		{
+			if (index >= _size)
+				throw std::out_of_range("Index out of bounds");
+		}
 
 		unsigned int size() const;
 };
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pauldos- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 11:26:22 by pauldos-          #+#    #+#             */
-/*   Updated: 2025/10/01 12:09:36 by pauldos-         ###   ########.fr       */
+/*   Updated: 2025/10/01 17:19:08 by pauldos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,27 @@ int main()
 	Array<int> a(5);
 	std::cout << "size: " << a.size() << std::endl;
 
+	a[0] = 20;
+	a[1] = 30;
+
+	const Array<int> f(2);
+	f[0] = 50;
+	f[1] = 80;
+
+
+	std::cout << "a[0] -> " << a[0] << ". a[1] -> " << a[1] << std::endl;
+	std::cout << "f[0] -> " << f[0] << ". f[1] -> " << f[1] << std::endl;
+
 	int *b = new int();
 	std::cout << b << std::endl;
 	delete b;
+
+	Array<int> c(a);
+	Array<int> d = a;
+
+	Array<int> e;
+	e = a;
+
 
 	return 0;
 }
