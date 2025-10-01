edcbbfa5ab9a68e8c8fa7ef36f20ad8f03ecf1a1
psergioprt <paulo.santossilva@gmail.com>
2025-10-01 08:07:10 +0100

    update cpp07 ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,33 +6,29 @@
 /*   By: pauldos- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 14:39:11 by pauldos-          #+#    #+#             */
-/*   Updated: 2025/09/30 16:03:03 by pauldos-         ###   ########.fr       */
+/*   Updated: 2025/10/01 07:44:21 by pauldos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
 
-#include <iostream>
+#include <cstddef>
 
+// non-const elements (modifiable)
 template <typename T>
-void iter(T arr[], size_t length, void (*f) (T &))
+void iter(T arr[], size_t length, void (*f)(T &))
 {
 	for (size_t i = 0; i < length; i++)
 		f(arr[i]);
 }
 
+// const elements (read-only)
 template <typename T>
-void iter(T arr[], size_t length, void (*f) (T const &))
+void iter(const T arr[], size_t length, void (*f) (T const &))
 {
 	for (size_t i = 0; i < length; i++)
 		f(arr[i]);
-}
-
-template <typename T>
-void print_element(T const& x)
-{
-	std::cout << x << std::endl;
 }
 
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pauldos- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 15:00:11 by pauldos-          #+#    #+#             */
-/*   Updated: 2025/09/30 16:40:15 by pauldos-         ###   ########.fr       */
+/*   Updated: 2025/10/01 08:05:51 by pauldos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 #include <iostream>
 #include <cstring>
 
-/*void increment_by_3(int &nb)
+void increment_by_3(int &nb)
 {
-	 nb += 3;
-}*/
+	nb += 3;
+}
 
-void increment_by_3(int nb[])
+template <typename T>
+void add_3(T &nb)
 {
-
-	 nb += 3;
+	nb += 3;
 }
 
 void to_lower(char &nb)
@@ -32,22 +32,42 @@ void to_lower(char &nb)
 		nb += 32;
 }
 
+template <typename T>
+void print_element(T const& x)
+{
+	std::cout << x << std::endl;
+}
+
+class
+
 int main()
 {
+	std::cout << "\nCONST INT ARRAY" << std::endl;
+	const int carr[] = {1, 2, 3, 4, 5};
+	size_t len = sizeof(carr) / sizeof(carr[0]);
+	iter(carr, len, print_element);
+
+	std::cout << "\nNON-CONST INT ARRAY" << std::endl;
 	int arr[] = {1, 2, 3, 4, 5};
-	size_t len = sizeof(arr) / sizeof(arr[0]);	
+	len = sizeof(arr) / sizeof(arr[0]);	
 	iter(arr, len, print_element);
 	
+	std::cout << "\nSTRING ARRAY" << std::endl;
 	std::string arr2[] = {"Hello", "World"};
 	len = sizeof(arr2) / sizeof(arr2[0]);
 	iter(arr2, len, print_element);
 
+	std::cout << "\nINT ARRAY UPDATING ELEMENTS +3" << std::endl;
 	int arr3[] = {1, 2, 3, 4, 5};
 	len = sizeof(arr3) / sizeof(arr3[0]);
 	iter(arr3, len, increment_by_3);
-	for (int i = 0; i < 5; i++)
-		std::cout << arr3[i] << std::endl;
+	iter(arr3, len, print_element);
+	
+	std::cout << "\nUSING TEMPLATE FUNCTION INT ARRAY UPDATE +3" << std::endl;
+	iter(arr3, len, add_3);
+	iter(arr3, len, print_element);
 
+	std::cout << "\nCHAR ARRAY" << std::endl;
 	char arr4[] = "Hello";
 	iter(arr4, strlen(arr4), to_lower);
 	std::cout << arr4 << std::endl;
